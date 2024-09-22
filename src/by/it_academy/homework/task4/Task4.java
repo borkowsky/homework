76f10e2349dfbdb80c465201c1beef3f4675fbba
Dzmitry Barkowski <alternativ1994@gmail.com>
2024-09-22 12:44:32 +0300

    Changed output in Task4.exercise1()

--- src/by/it_academy/homework/task4/Task4.java
+++ src/by/it_academy/homework/task4/Task4.java
@@ -18,10 +18,10 @@ public class Task4 {
 
     private static void exercise1() {
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Таблица умножения числа " + i);
             for (int j = 1; j <= 10; j++) {
-                System.out.println(i + " * " + j + " = " + i * j);
+                System.out.print((i * j) + "\t");
             }
+            System.out.println();
         }
     }
 
